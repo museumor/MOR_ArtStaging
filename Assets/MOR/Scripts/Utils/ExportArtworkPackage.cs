dfb3aec518967735d3b446b648ac0e69284c3ac6
Lindsay <lindsayjorgensen@museumor.com>
2020-04-29 13:43:15 -0700

    generate a name for package based on username and date

--- Assets/MOR/Scripts/Utils/ExportArtworkPackage.cs
+++ Assets/MOR/Scripts/Utils/ExportArtworkPackage.cs
@@ -1,12 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 namespace MOR.Museum {
 	public class ExportArtworkPackage : MonoBehaviour {
-
-
-
+		
 		public static void CountProjectVertices(){
 			GameObject g = GameObject.Find("YOUR_ART_HERE");
 			Validator.CheckVertexCount(g);
@@ -18,7 +17,10 @@ namespace MOR.Museum {
 			//ResourceValidation.
 
 			ExportPackageOptions exportPackageOptions = ExportPackageOptions.Recurse;
-			string exportFilename = "testname.unitypackage"; //TODO : set up somewhere to set this... prefab name? folder name? hmm....
+			DateTime thisDay = DateTime.Today;
+			string prefix = "MORStagedArtwork";
+			string datestamp = $"{thisDay.Year}-{thisDay.Month}-{thisDay.Day}";
+			string exportFilename = $"{prefix}_{Environment.UserName}_{datestamp}.unitypackage"; //TODO : set up somewhere to set this... prefab name? folder name? hmm....
 			string projectPath = Directory.GetCurrentDirectory();
 			exportFilename = Path.Combine(projectPath, exportFilename);
 			AssetDatabase.ExportPackage("Assets/YOUR_ASSETS_HERE", exportFilename, exportPackageOptions);
