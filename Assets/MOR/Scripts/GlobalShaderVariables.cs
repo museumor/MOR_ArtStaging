84af52615deb45d800101be75cbc4cf984e169f0
Lindsay <lindsayjorgensen@museumor.com>
2020-10-26 10:38:57 -0700

    Add scripts to mock access to Server time if relevant for the piece

---
+++ Assets/MOR/Scripts/GlobalShaderVariables.cs
@@ -0,0 +1,39 @@
+﻿using MOR.Industries;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class GlobalShaderVariables : MonoBehaviour {
+	private static readonly int ServerTimeProp = Shader.PropertyToID("_ServerTime");
+
+	private void Awake() {
+		//MOREventManager.Listen(MOREvents.SERVER_CONNECT, SetServerTime);
+		SetServerTime();
+	}
+
+	private void OnDisable() {
+		//MOREventManager.Remove(MOREvents.SERVER_CONNECT,SetServerTime);
+	}
+	private const double WRAP_POINT = (9*60*60)/3;//~read 9 hours is the limit for floats, so wrap over one third of that as we * 3 , so (9*60*60)/3
+	/*private void Update() {
+		Vector4 times;
+		serverTimeOffset += Time.deltaTime;
+		serverTimeOffset = serverTimeOffset % WRAP_POINT;
+		float time = (float)(serverTimeOffset);
+		times.y = time;
+		sentTime = times.y;
+		times.x = (float)(time / 20.0);
+		times.z = (float)((time * 2.0) % float.MaxValue);
+		times.w = (float)((time * 3.0) % float.MaxValue);
+		Shader.SetGlobalVector(ServerTimeProp, times);
+	}*/
+	private double serverTimeOffset = 0;
+	private float sentTime;
+	private void SetServerTime(params object[] args) {
+		double setTime = Time.timeSinceLevelLoad;
+		if(Application.isPlaying) {
+			setTime -= MORRealtime.serverTime%WRAP_POINT;
+		}
+		serverTimeOffset = setTime;
+		Shader.SetGlobalFloat(ServerTimeProp, (float) serverTimeOffset);
+	}
+}
---
+++ Assets/MOR/Scripts/MORRealtime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace MOR.Industries {
+	public class MORRealtime : Singleton<MORRealtime>{
+		public static double serverTime
+		{
+		get
+		{
+			//if ((!Globals.applicationQuitting) && (MORRealtime.instance.realtimeFound) && (MORRealtime.instance.realtime.connected)) {
+				
+				//return MORRealtime.instance.realtime.room.time;
+			//}
+			//else {
+				// ???? - I kind of want this error in here, but we've got some art pieces that are trying to use this before we've connected, so we probably need to fix that first
+				//Util.LogWarning("MORRealtime: Trying to get time from a non-existant room");
+				return Time.realtimeSinceStartup;
+			//}
+		}
+	}
+}
+
+}
